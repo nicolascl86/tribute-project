31dc29df6826f91874086affdabc8f1d76d75456
NicolasCl86 <n.d.kleovoulou@gmail.com>
2025-04-30 05:02:40 +0300

    Add Change Mood JS

--- app.js
+++ app.js
@@ -8,3 +8,7 @@ button.addEventListener('click', () => {
 
 const now = new Date();
 document.getElementById('date-time').textContent = now.toLocaleString();
+
+document.getElementById('change-color').addEventListener('click', () => {
+    document.body.style.backgroundColor = '#fdf6e3';
+});
